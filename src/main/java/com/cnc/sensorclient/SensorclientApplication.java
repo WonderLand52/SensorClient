fd5b66cf92c603b1daffd2e74430e5954acc1e4d
Illia <illiarudenko52@gmail.com>
2017-06-09 13:48:40 +0300

    Initial commit

---
+++ src/main/java/com/cnc/sensorclient/SensorclientApplication.java
@@ -0,0 +1,12 @@
+package com.cnc.sensorclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorclientApplication.class, args);
+	}
+}
